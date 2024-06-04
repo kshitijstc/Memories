d6a8168ab31ac58f462a70b46fdca59876626ae6
kshitijstc <kshitijstc@gmail.com>
2024-06-04 11:36:54 +0530

    css changed title changed

--- server/index.js
+++ server/index.js
@@ -13,6 +13,9 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("Hello to Memories server");
+});
 app.use("/posts", postRoutes);
 
 const PORT = process.env.PORT;
