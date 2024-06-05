86a85712ef4d463e7b3b6ed1005c2ad46a1740b6
kshitijstc <kshitijstc@gmail.com>
2024-06-05 22:33:49 +0530

    signIn signUp added

--- client/src/components/Auth/Auth.jsx
+++ client/src/components/Auth/Auth.jsx
@@ -1,9 +1,154 @@
-import React from 'react'
+import React, { useState } from "react";
+import {
+  Container,
+  Paper,
+  Avatar,
+  Typography,
+  Grid,
+  Button,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
+import Input from "./Input";
 
 const Auth = () => {
-    const state=null;
+  const [showPassword, setShowPassword] = useState(false);
+  const [isSignup, setIsSignup] = useState(false);
+  const state = null;
+  
+  const handleSubmit = () => {};
+  const handleChange = () => {};
+  const handleShowPassword = () => setShowPassword(!showPassword);
+  const switchMode = () => {
+    setIsSignup((prevIsSignup) => !prevIsSignup);
+    setShowPassword(false);
+  }
   return (
-    <div>Auth</div>
+    <Container component="main" maxWidth="xs">
+      <Paper elevation={3} sx={{ padding: '20px', marginTop: '30px' }}>
+        <Avatar 
+          sx={{
+            margin: "10px auto 20px auto",
+            backgroundColor: "primary.main",
+          }}
+        >
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography
+          variant="h5"
+          sx={{textAlign: "center",}}
+        >
+          {isSignup ? "Sign up" : "Sign in"}
+        </Typography>
+        <form onSubmit={handleSubmit}>
+          <Grid
+            container
+            spacing={3}
+            sx={{
+              padding: "20px",
+              textAlign: "center"}}
+          >
+            {isSignup && (
+              <>
+                <Input
+                  name="firstName"
+                  label="First Name"
+                  handleChange={handleChange}
+                  autoFocus
+                  half
+                />
+                <Input
+                  name="lastName"
+                  label="Last Name"
+                  handleChange={handleChange}
+                  half
+                />
+              </>
+            )}
+            <Input
+              name="email"
+              label="Email Address"
+              handleChange={handleChange}
+              type="email"
+            />
+            <Input
+              name="password"
+              label="Password"
+              handleChange={handleChange}
+              type={showPassword ? "text" : "password"}
+              handleShowPassword={handleShowPassword}
+            />
+            {isSignup && (
+              <Input
+                name="confirmPassword"
+                label="Repeat Password"
+                handleChange={handleChange}
+                type="password"
+              />
+            )}
+          </Grid>
+          <Button type="submit" fullWidth variant="contained" color="primary" 
+            sx={{ 
+            margin: "10px 0",
+            fontSize: "16px",
+            fontWeight: "bold",
+            letterSpacing: "1px",
+            }}
+          >
+            {isSignup ? "Sign Up" : "Sign In"}
+          </Button>
+          
+          {/* <GoogleOAuthProvider clientId="YOUR_CLIENT_ID">
+            <GoogleLogin
+              onSuccess={(response) => {
+                console.log(response);
+              }}
+              onFailure={(response) => {
+                console.log(response);
+              }}
+              render={(renderProps) => (
+                <Button
+                  className={classes.googleButton}
+                  color="primary"
+                  fullWidth
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                  startIcon={<Icon />}
+                  variant="contained"
+                >
+                  Google Sign In
+                </Button>
+              )}
+            />
+            <GoogleLogout
+              render={(renderProps) => (
+                <Button
+                  className={classes.logout}
+                  color="primary"
+                  fullWidth
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                  startIcon={<Icon />}
+                  variant="contained"
+                >
+                  Logout
+                </Button>
+              )}
+            />
+          </GoogleOAuthProvider> */}
+          <Grid container justify="flex-end">
+            <Grid item>
+              <Button onClick={switchMode}>
+                {isSignup
+                  ? "Already have an account? Sign in"
+                  : "Don't have an account? Sign Up"}
+              </Button>
+            </Grid>
+          </Grid>
+        </form>
+      </Paper>
+    </Container>
   );
 };
 
---
+++ client/src/components/Auth/Input.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { TextField, Grid, InputAdornment, IconButton } from '@mui/material'
+import { Visibility, VisibilityOff } from '@mui/icons-material'
+
+
+const Input = ({name,label,type,half,handleChange,autoFocus,handleShowPassword}) => {
+  return (
+    <Grid item xs={12} sm={half ? 6 : 12}>
+        <TextField
+            name={name}
+            onChange={handleChange}
+            variant="outlined"
+            required
+            fullWidth
+            type={type}
+            label={label}
+            autoFocus={autoFocus}
+            InputProps={name==='password' ? {
+                endAdornment: (
+                    <InputAdornment position="end">
+                        <IconButton onClick={handleShowPassword}>
+                            {type==='password' ? <Visibility /> : <VisibilityOff />}
+                        </IconButton>
+                    </InputAdornment>
+                ),
+            } : null}
+        />
+    </Grid>
+  );
+};
+
+export default Input;
