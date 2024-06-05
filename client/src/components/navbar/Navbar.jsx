04a34257edaab00da75be2980aacc6634e6916c6
kshitijstc <kshitijstc@gmail.com>
2024-06-05 16:34:44 +0530

    frontend render

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -3,12 +3,13 @@ import { Link } from "react-router-dom";
 import { AppBar, Avatar, Toolbar, Typography, Button } from "@mui/material";
 import memories from "../../images/memories.jpg";
 import "../../app.css";
+
 const Navbar = () => {
   const user = null;
-    return (
+  return (
     <AppBar className="appBar" position="static" color="inherit">
-      <div className="classesbrandcontainer">
-        <div className="appBarContent">
+      <Toolbar className="toolbar">
+        <div className="logo-container">
           <Typography
             component={Link}
             to="/"
@@ -20,34 +21,34 @@ const Navbar = () => {
           </Typography>
           <img className="image" src={memories} alt="memories" height="70" />
         </div>
-      </div>
-      <Toolbar className="toolbar">
-        {user ? (
-          <div className="profile">
-            <Avatar
-              className="avatar"
-              alt={user.result.name}
-              src={user.result.imageUrl}
+        <div className="toolbar-buttons">
+          {user ? (
+            <div className="profile">
+              <Avatar
+                className="avatar"
+                alt={user.result.name}
+                src={user.result.imageUrl}
+              >
+                {user.result.name.charAt(0)}
+              </Avatar>
+              <Typography className="userName" variant="h6">
+                {user.result.name}
+              </Typography>
+              <Button className="logout" variant="contained" color="secondary">
+                Logout
+              </Button>
+            </div>
+          ) : (
+            <Button
+              component={Link}
+              to="/auth"
+              variant="contained"
+              color="primary"
             >
-              {user.result.name.charAt(0)}
-            </Avatar>
-            <Typography className="userName" variant="h6">
-              {user.result.name}
-            </Typography>
-            <Button className="logout" variant="contained" color="secondary">
-              Logout
+              Sign In
             </Button>
-          </div>
-        ) : (
-          <Button
-            component={Link}
-            to="/auth"
-            variant="contained"
-            color="primary"
-          >
-            Sign In
-          </Button>
-        )}
+          )}
+        </div>
       </Toolbar>
     </AppBar>
   );
