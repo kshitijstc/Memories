94d06964fd616326169bddd6990bb1deb9edeea7
kshitijstc <kshitijstc@gmail.com>
2024-06-09 08:34:42 +0530

    Applied LIFO to posts on server side(more effective)

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -9,13 +9,14 @@ import Post from './post/Post'; // Import the Post component
 const Posts = ({setCurrentId}) => {
   // useSelector is used to fetch data from redux store
   const posts = useSelector((state) => state.posts);
-  const sortedPosts = posts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
-  console.log(sortedPosts);
+  console.log(posts);
+  // const sortedPosts = posts.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
+  
   return (
     <div>
       {!posts.length ? <CircularProgress /> : (
         <Grid container spacing={3}>
-          {sortedPosts.map((post) => (
+          {posts.map((post) => (
             <Grid key={post._id} item xs={12} sm={10} md={6}>
               <Post post={post} setCurrentId={setCurrentId}/>
             </Grid>
--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 export const getPosts = async (req,res) => {
     try {
-        const postMessages = await PostMessage.find();
+        const postMessages = await PostMessage.find().sort({ createdAt: -1 });
         console.log(postMessages);
         res.status(200).json(postMessages);
     } catch (error) {
